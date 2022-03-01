e31e2bd4f7f95a497783b2608347ce051cabc830
Roger1DAM <94872585+Roger1DAM@users.noreply.github.com>
2022-03-01 20:12:23 +0100

    Add files via upload

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,3 +1,27 @@
+//if(document.querySelectorAll('.btn-reserva')){
+    const buttonsReserva = document.querySelectorAll('.btn-reserva');
+    buttonsReserva.forEach(btn => {
+        btn.addEventListener('click', function(){
+            const id = btn.closest('#idActivitat');
+            console.log("ID: " + id);
+            reservar();
+        });
+    });
+//}
+
+function reservar() {
+    const id = "";
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('GET','reserva.php',true);
+    xhttp.send();
+    xhttp.onreadystatechange = function(){
+        if(this,this.readyState == 4 && this.status == 200){
+            let result = this.response;
+            console.log(result);
+        }
+    }
+}
+
 
 
 
